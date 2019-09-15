2ea296d1e2a6fe4588a286f7e47bb5d9838b206e
ericz3 <ericz3@illinois.edu>
2019-09-15 16:11:57 -0500

    backend challenge completed

--- backend/app.py
+++ backend/app.py
@@ -18,7 +18,6 @@ def create_response(
     IMPORTANT: data must be a dictionary where:
     - the key is the name of the type of data
     - the value is the data itself
-
     :param data <str> optional data
     :param status <int> optional status code, defaults to 200
     :param message <str> optional message
@@ -53,17 +52,54 @@ def mirror(name):
 
 @app.route("/contacts", methods=['GET'])
 def get_all_contacts():
-    return create_response({"contacts": db.get('contacts')})
+    contacts = db.get('contacts')
+    args = request.args
+    if "hobby" in args:
+        hobby = args["hobby"]
+        filtered_contacts = [c for c in contacts if c['hobby'] == hobby]
+        if not filtered_contacts:
+            return create_response(status=404, message="No contact with this hobby exists")
+        return create_response({"contacts": filtered_contacts})
+    return create_response({"contacts": contacts})
 
-@app.route("/shows/<id>", methods=['DELETE'])
-def delete_show(id):
+@app.route("/contacts/<id>", methods=['DELETE'])
+def delete_contact(id):
     if db.getById('contacts', int(id)) is None:
         return create_response(status=404, message="No contact with this id exists")
     db.deleteById('contacts', int(id))
     return create_response(message="Contact deleted")
 
+@app.route("/contacts/<id>", methods=['GET'])
+def get_contact(id):
+    if db.getById('contacts', int(id)) is None:
+        return create_response(status=404, message="No contact with this id exists")
+    return create_response(db.getById('contacts', int(id)))
 
-# TODO: Implement the rest of the API here!
+@app.route("/contacts", methods=['POST'])
+def post_contact():
+    data = request.json
+    if not "name" in data:
+        return create_response(status=422, message="A name must be provided")
+    if not "nickname" in data:
+        return create_response(status=422, message="A nickname must be provided")
+    if not "hobby" in data:
+        return create_response(status=422, message="A hobby must be provided")
+    db.create('contacts', data)
+    return create_response(data, status=201)
+
+@app.route("/contacts/<id>", methods=['PUT'])
+def update_contact(id):
+    if db.getById('contacts', int(id)) is None:
+        return create_response(status=404, message="No contact with this id exists")
+    data = request.json
+    keys = list(data.keys())
+    for key in keys:
+        if (key != "name") and (key != "hobby"):
+            del data[key]
+    db.updateById('contacts', int(id), data)
+    return create_response(db.getById('contacts', int(id)), status=201)
+
+
 
 """
 ~~~~~~~~~~~~ END API ~~~~~~~~~~~~
